09f49615ea53a1e5d244bf5afaab061e3e82d92e
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-02-17 11:55:29 -0700

    trying to add header files

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include "Parameter.h"
+#include "Predicate.h"
+#include "Rule.h"
+#include "DatalogProgram.h"
+int main()
+{
+    printf("hello from %s!\n", "Parser");
+    return 0;
+}
