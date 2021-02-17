04c09b34b6818a1fb5e3a44afe2f61a967e5a611
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-02-17 15:48:05 -0700

    building main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
-#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #include "Parameter.h"
 #include "Predicate.h"
 #include "Rule.h"
 #include "DatalogProgram.h"
-int main()
+#include "Lexer.h"
+#include "Token.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-    printf("hello from %s!\n", "Parser");
+    string fileName = argv[1];
+    ifstream input;
+    string file;
+    vector <Token*> lexInput;
+    Lexer program = Lexer();
+
+    input.open(fileName);
+
+    if (input.fail())
+    {
+        cerr << "File Read Error" << endl;
+    }
+    else
+    {
+        for (int i = 0; input.peek() != EOF; i++)
+        {
+            file = file += input.get();
+        }
+    }
+    input.close();
+
+    lexInput = program.Run(file);
+
+    for (int i = 0; i < lexInput.size(); i++)
+    {
+        if (lexInput.at(i).getType() == COMMENT)
+        {
+
+        }
+    }
+
     return 0;
 }
