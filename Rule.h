054713c9a162dbb4427545b9d1b48a9c0ac7d33a
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-02-23 09:18:17 -0700

    final initialization bugs

--- Rule.h
+++ Rule.h
@@ -22,9 +22,7 @@ public:
 	}
 
 	void AddPredicate(string id) {
-		Predicate create = Predicate(id);
-		Predicate* add -> create;
-		predicateList.push_back(add);
+		predicateList.push_back(new Predicate(id));
 	}
 
 	void  AddContent(Token* add)
