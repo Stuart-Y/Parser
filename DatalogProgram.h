74eb902e6e1f981ba14f149786092d1c860648a4
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-02-24 12:50:34 -0700

    found problem, requesting parameters from unnitialized arrays

--- DatalogProgram.h
+++ DatalogProgram.h
@@ -47,49 +47,41 @@ public:
 	}
 
 	void AddID(Token* data) {
-		if (schemes[schemes.size() - 1]->getName() == "NULL" && queries.size() == 0)
+		if (queries.size() != 0)
 		{
-			cout << "case 1";
+			queries[queries.size() - 1]->AddContents(data);
+		}
+		else if (schemes[schemes.size() - 1]->getName() == "NULL" && queries.size() == 0)
+		{
 			schemes[schemes.size() - 1]->AddName(data->inputString());
 		}
 		else if (facts.size() < 1 && queries.size() == 0)
 		{
-			cout << "case 2";
 			schemes[schemes.size() - 1]->AddContents(data);
 		}
 		else if (facts[facts.size() - 1]->getName() == "NULL" && queries.size() == 0)
 		{
-			cout << "case 3";
 			facts[facts.size() - 1]->AddName(data->inputString());
 		}
 		else if (rules[rules.size() - 1]->getHeadName() == "NULL" && queries.size() == 0)
 		{
-			cout << "case 4";
 			rules[rules.size() - 1]->AddHeadID(data->inputString());
 		}
 		else if (rules[rules.size() - 1]->getListSize() < 1 && queries.size() == 0)
 		{
-			cout << "case 5";
 			rules[rules.size() - 1]->AddHeadContent(data);
 		}
 		else if (rules[rules.size() - 1]->needName() && queries.size() == 0)
 		{
-			cout << "case 6";
 			rules[rules.size() - 1]->AddID(data->inputString());
 		}
 		else if (queries.size() == 0 )
 		{
-			cout << "case 7";
 			rules[rules.size() - 1]->AddContent(data);
 		}
 		else if (queries[queries.size() - 1]->getName() == "NULL")
 		{
-			cout << "case 8";
 			queries[queries.size() - 1]->AddName(data->inputString());
-		}
-		else if (queries.size() > 0) {
-			cout << "case 9";
-			queries[queries.size() - 1]->AddContents(data);
 		}
 		else {
 			cout << "ERROR: Improper ID assignment" << "\n";
